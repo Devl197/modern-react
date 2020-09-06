6f8741300f29da387578dce9da0b6f57a36caaa9
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-09-06 11:04:42 +0200

    Adding thunk for creating todos on server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,4 @@
-import { loadTodosFailure, loadTodosInProgress, loadTodosSuccess} from './actions';
+import { loadTodosFailure, loadTodosInProgress, loadTodosSuccess, createTodo} from './actions';
 
 export const displayAlert = text => () => {
     alert(text);
@@ -12,6 +12,23 @@ export const loadTodos = () => async (dispatch, getState) => {
         dispatch(loadTodosSuccess(todos));
     } catch(e){
         dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    } 
+}
+
+export const addTodoRequest = text => async (dispatch) => {
+    try{
+        const body = JSON.stringify({text});
+        const response = await fetch('http://localhost:8080/todos', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'post',
+        body
+        });
+        const todo = await response.json();
+        dispatch(createTodo(todo));
+    } catch(e) {
         dispatch(displayAlert(e));
     } 
 }
